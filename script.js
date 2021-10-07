1b9bbffeaa77cb8e8b29c216c4b45fa354a5d3b1
Mohammed Musaib Ali <64194019+mdmusaibali@users.noreply.github.com>
2021-10-07 19:16:32 +0530

    Bigger Update with smart scripts

--- script.js
+++ script.js
@@ -1,61 +1,189 @@
-const booksBoxAi = document.querySelector(".books-box-ai");
-const booksBoxSet = document.querySelector(".books-box-set");
-const booksBoxDbms = document.querySelector(".books-box-dbms");
-const booksBoxDs = document.querySelector(".books-box-ds");
-const booksBoxDc = document.querySelector(".books-box-dc");
+// const booksBoxAi = document.querySelector(".books-box-ai");
+// const booksBoxSet = document.querySelector(".books-box-set");
+// const booksBoxDbms = document.querySelector(".books-box-dbms");
+// const booksBoxDs = document.querySelector(".books-box-ds");
+// const booksBoxDc = document.querySelector(".books-box-dc");
+// const booksBoxCn = document.querySelector(".books-box-cn");
+// const booksBoxMl = document.querySelector(".books-box-ml");
+// const booksBoxFafl = document.querySelector(".books-box-fafl");
+// const booksBoxCg = document.querySelector(".books-box-cg");
+// const booksBoxCis = document.querySelector(".books-box-cis");
+// const booksBoxDip = document.querySelector(".books-box-dip");
+// const booksBoxEs = document.querySelector(".books-box-es");
+// const booksBoxDa = document.querySelector(".books-box-da");
+// const booksBoxRpa = document.querySelector(".books-box-rpa");
+// const booksBoxPp = document.querySelector(".books-box-pp");
+// const booksBoxAjj = document.querySelector(".books-box-ajj");
+// const booksBoxIot = document.querySelector(".books-box-iot");
+// const booksBoxWst = document.querySelector(".books-box-wst");
 
-const setTrigger = document.querySelector(".set-trigger");
-const aiTrigger = document.querySelector(".ai-trigger");
-const dbmsTrigger = document.querySelector(".dbms-trigger");
-const dsTrigger = document.querySelector(".ds-trigger");
-const dcTrigger = document.querySelector(".dc-trigger");
+// const setTrigger = document.querySelector(".set-trigger");
+// const aiTrigger = document.querySelector(".ai-trigger");
+// const dbmsTrigger = document.querySelector(".dbms-trigger");
+// const dsTrigger = document.querySelector(".ds-trigger");
+// const dcTrigger = document.querySelector(".dc-trigger");
+// const cnTrigger = document.querySelector(".cn-trigger");
+// const mlTrigger = document.querySelector(".ml-trigger");
+// const faflTrigger = document.querySelector(".fafl-trigger");
+// const cgTrigger = document.querySelector(".cg-trigger");
+// const cisTrigger = document.querySelector(".cis-trigger");
+// const dipTrigger = document.querySelector(".dip-trigger");
+// const esTrigger = document.querySelector(".es-trigger");
+// const daTrigger = document.querySelector(".da-trigger");
+// const rpaTrigger = document.querySelector(".rpa-trigger");
+// const ppTrigger = document.querySelector(".pp-trigger");
+// const ajjTrigger = document.querySelector(".ajj-trigger");
+// const iotTrigger = document.querySelector(".iot-trigger");
+// const wstTrigger = document.querySelector(".wst-trigger");
 
+// const downArrowAi = document.querySelector(".down-arrow-ai");
+// const upArrowAi = document.querySelector(".up-arrow-ai");
+// const downArrowSet = document.querySelector(".down-arrow-set");
+// const upArrowSet = document.querySelector(".up-arrow-set");
+// const downArrowDbms = document.querySelector(".down-arrow-dbms");
+// const upArrowDbms = document.querySelector(".up-arrow-dbms");
+// const downArrowDs = document.querySelector(".down-arrow-ds");
+// const upArrowDs = document.querySelector(".up-arrow-ds");
+// const downArrowDc = document.querySelector(".down-arrow-dc");
+// const upArrowDc = document.querySelector(".up-arrow-dc");
+// const downArrowCn = document.querySelector(".down-arrow-cn");
+// const upArrowCn = document.querySelector(".up-arrow-cn");
+// const downArrowMl = document.querySelector(".down-arrow-ml");
+// const upArrowMl = document.querySelector(".up-arrow-ml");
+// const downArrowFafl = document.querySelector(".down-arrow-fafl");
+// const upArrowFafl = document.querySelector(".up-arrow-fafl");
+// const downArrowCg = document.querySelector(".down-arrow-cg");
+// const upArrowCg = document.querySelector(".up-arrow-cg");
+// const downArrowCis = document.querySelector(".down-arrow-cis");
+// const upArrowCis = document.querySelector(".up-arrow-cis");
+// const downArrowDip = document.querySelector(".down-arrow-dip");
+// const upArrowDip = document.querySelector(".up-arrow-dip");
+// const downArrowEs = document.querySelector(".down-arrow-es");
+// const upArrowEs = document.querySelector(".up-arrow-es");
+// const downArrowDa = document.querySelector(".down-arrow-da");
+// const upArrowDa = document.querySelector(".up-arrow-da");
+// const downArrowRpa = document.querySelector(".down-arrow-rpa");
+// const upArrowRpa = document.querySelector(".up-arrow-rpa");
+// const downArrowPp = document.querySelector(".down-arrow-pp");
+// const upArrowPp = document.querySelector(".up-arrow-pp");
+// const downArrowAjj = document.querySelector(".down-arrow-ajj");
+// const upArrowAjj = document.querySelector(".up-arrow-ajj");
+// const downArrowIot = document.querySelector(".down-arrow-iot");
+// const upArrowIot = document.querySelector(".up-arrow-iot");
+// const downArrowWst = document.querySelector(".down-arrow-wst");
+// const upArrowWst = document.querySelector(".up-arrow-wst");
+
+// aiTrigger.addEventListener("click", function () {
+//   booksBoxAi.classList.toggle("hide");
+//   booksBoxAi.classList.toggle("unhide");
+//   downArrowAi.classList.toggle("hidden");
+//   upArrowAi.classList.toggle("hidden");
+// });
+// setTrigger.addEventListener("click", function () {
+//   booksBoxSet.classList.toggle("hide");
+//   booksBoxSet.classList.toggle("unhide");
+//   downArrowSet.classList.toggle("hidden");
+//   upArrowSet.classList.toggle("hidden");
+// });
+// dbmsTrigger.addEventListener("click", function () {
+//   booksBoxDbms.classList.toggle("hide");
+//   booksBoxDbms.classList.toggle("unhide");
+//   downArrowDbms.classList.toggle("hidden");
+//   upArrowDbms.classList.toggle("hidden");
+// });
+// dsTrigger.addEventListener("click", function () {
+//   booksBoxDs.classList.toggle("hide");
+//   booksBoxDs.classList.toggle("unhide");
+//   downArrowDs.classList.toggle("hidden");
+//   upArrowDs.classList.toggle("hidden");
+// });
+// dcTrigger.addEventListener("click", function () {
+//   booksBoxDc.classList.toggle("hide");
+//   booksBoxDc.classList.toggle("unhide");
+//   downArrowDc.classList.toggle("hidden");
+//   upArrowDc.classList.toggle("hidden");
+// });
+// cnTrigger.addEventListener("click", function () {
+//   booksBoxCn.classList.toggle("hide");
+//   booksBoxCn.classList.toggle("unhide");
+//   downArrowCn.classList.toggle("hidden");
+//   upArrowCn.classList.toggle("hidden");
+// });
+// mlTrigger.addEventListener("click", function () {
+//   booksBoxMl.classList.toggle("hide");
+//   booksBoxMl.classList.toggle("unhide");
+//   downArrowMl.classList.toggle("hidden");
+//   upArrowMl.classList.toggle("hidden");
+// });
+// faflTrigger.addEventListener("click", function () {
+//   booksBoxFafl.classList.toggle("hide");
+//   booksBoxFafl.classList.toggle("unhide");
+//   downArrowFafl.classList.toggle("hidden");
+//   upArrowFafl.classList.toggle("hidden");
+// });
+// cgTrigger.addEventListener("click", function () {
+//   booksBoxCg.classList.toggle("hide");
+//   booksBoxCg.classList.toggle("unhide");
+//   downArrowCg.classList.toggle("hidden");
+//   upArrowCg.classList.toggle("hidden");
+// });
+// cisTrigger.addEventListener("click", function () {
+//   booksBoxCis.classList.toggle("hide");
+//   booksBoxCis.classList.toggle("unhide");
+//   downArrowCis.classList.toggle("hidden");
+//   upArrowCis.classList.toggle("hidden");
+// });
+// dipTrigger.addEventListener("click", function () {
+//   booksBoxDip.classList.toggle("hide");
+//   booksBoxDip.classList.toggle("unhide");
+//   downArrowDip.classList.toggle("hidden");
+//   upArrowDip.classList.toggle("hidden");
+// });
+// esTrigger.addEventListener("click", function () {
+//   booksBoxEs.classList.toggle("hide");
+//   booksBoxEs.classList.toggle("unhide");
+//   downArrowEs.classList.toggle("hidden");
+//   upArrowEs.classList.toggle("hidden");
+// });
+// daTrigger.addEventListener("click", function () {
+//   booksBoxDa.classList.toggle("hide");
+//   booksBoxDa.classList.toggle("unhide");
+//   downArrowDa.classList.toggle("hidden");
+//   upArrowDa.classList.toggle("hidden");
+// });
+// rpaTrigger.addEventListener("click", function () {
+//   booksBoxRpa.classList.toggle("hide");
+//   booksBoxRpa.classList.toggle("unhide");
+//   downArrowRpa.classList.toggle("hidden");
+//   upArrowRpa.classList.toggle("hidden");
+// });
+// ppTrigger.addEventListener("click", function () {
+//   booksBoxPp.classList.toggle("hide");
+//   booksBoxPp.classList.toggle("unhide");
+//   downArrowPp.classList.toggle("hidden");
+//   upArrowPp.classList.toggle("hidden");
+// });
+// ajjTrigger.addEventListener("click", function () {
+//   booksBoxAjj.classList.toggle("hide");
+//   booksBoxAjj.classList.toggle("unhide");
+//   downArrowAjj.classList.toggle("hidden");
+//   upArrowAjj.classList.toggle("hidden");
+// });
+// iotTrigger.addEventListener("click", function () {
+//   booksBoxIot.classList.toggle("hide");
+//   booksBoxIot.classList.toggle("unhide");
+//   downArrowIot.classList.toggle("hidden");
+//   upArrowIot.classList.toggle("hidden");
+// });
+// wstTrigger.addEventListener("click", function () {
+//   booksBoxWst.classList.toggle("hide");
+//   booksBoxWst.classList.toggle("unhide");
+//   downArrowWst.classList.toggle("hidden");
+//   upArrowWst.classList.toggle("hidden");
+// });
 const closeButton = document.querySelector(".close-icon");
 const overflow = document.querySelector(".overflows");
 const body = document.querySelector("body");
-
-const downArrowAi = document.querySelector(".down-arrow-ai");
-const upArrowAi = document.querySelector(".up-arrow-ai");
-const downArrowSet = document.querySelector(".down-arrow-set");
-const upArrowSet = document.querySelector(".up-arrow-set");
-const downArrowDbms = document.querySelector(".down-arrow-dbms");
-const upArrowDbms = document.querySelector(".up-arrow-dbms");
-const downArrowDs = document.querySelector(".down-arrow-ds");
-const upArrowDs = document.querySelector(".up-arrow-ds");
-const downArrowDc = document.querySelector(".down-arrow-dc");
-const upArrowDc = document.querySelector(".up-arrow-dc");
-
-aiTrigger.addEventListener("click", function () {
-  booksBoxAi.classList.toggle("hide");
-  booksBoxAi.classList.toggle("unhide");
-  downArrowAi.classList.toggle("hidden");
-  upArrowAi.classList.toggle("hidden");
-});
-setTrigger.addEventListener("click", function () {
-  booksBoxSet.classList.toggle("hide");
-  booksBoxSet.classList.toggle("unhide");
-  downArrowSet.classList.toggle("hidden");
-  upArrowSet.classList.toggle("hidden");
-});
-dbmsTrigger.addEventListener("click", function () {
-  booksBoxDbms.classList.toggle("hide");
-  booksBoxDbms.classList.toggle("unhide");
-  downArrowDbms.classList.toggle("hidden");
-  upArrowDbms.classList.toggle("hidden");
-});
-dsTrigger.addEventListener("click", function () {
-  booksBoxDs.classList.toggle("hide");
-  booksBoxDs.classList.toggle("unhide");
-  downArrowDs.classList.toggle("hidden");
-  upArrowDs.classList.toggle("hidden");
-});
-dcTrigger.addEventListener("click", function () {
-  booksBoxDc.classList.toggle("hide");
-  booksBoxDc.classList.toggle("unhide");
-  downArrowDc.classList.toggle("hidden");
-  upArrowDc.classList.toggle("hidden");
-});
-
 closeButton.addEventListener("click", function () {
   overflow.classList.add("hide");
   body.style.overflow = "scroll";
@@ -68,3 +196,17 @@ const downloadButtons = document.querySelectorAll(".btn").forEach((btn) =>
     document.documentElement.scrollTop = 0;
   })
 );
+
+//------------------------------------------------------------------------------------
+const box = document.querySelectorAll(".box");
+const downArrow = document.querySelectorAll(".down-arrow");
+const upArrow = document.querySelectorAll(".up-arrow");
+const trigger = document.querySelectorAll(".trigger");
+for (let i = 0; i < trigger.length; i++) {
+  trigger[i].addEventListener("click", function () {
+    box[i].classList.toggle("hide");
+    box[i].classList.toggle("unhide");
+    downArrow[i].classList.toggle("hidden");
+    upArrow[i].classList.toggle("hidden");
+  });
+}
